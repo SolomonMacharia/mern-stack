2d780903869a76901cd718074d9fb9157e720277
Solomon Macharia <macha@Solomons-MacBook-Pro.local>
2024-09-12 04:29:52 +0300

    creates db connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
@@ -7,7 +8,8 @@ dotenv.config()
 
 const port = process.env.PORT;
 
-console.log(process.env.MONGO_URI)
-
-app.listen(port || 5000 , () => console.log(`listening on port ${port}`));
+app.listen(port || 5000 , () => {
+    connectDB();
+    console.log(`listening on port ${port}`)
+});
 
